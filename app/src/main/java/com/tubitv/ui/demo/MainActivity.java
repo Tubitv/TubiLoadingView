c06d0ef027c345cfbce7d641b6422087a76fa049
StoyanD <s.dimitrov19@gmail.com>
2017-04-24 13:52:42 -0700

    -added methods to toggle the view

--- app/src/main/java/com/tubitv/ui/demo/MainActivity.java
+++ app/src/main/java/com/tubitv/ui/demo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.tubitv.ui.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.tubitv.ui.TubiLoadingView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TubiLoadingView loadingView = (TubiLoadingView) findViewById(R.id.tubiLoadingView2);
+        Button button = (Button) findViewById(R.id.toggle);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingView.toggle();
+            }
+        });
     }
 }
--- lib/src/main/java/com/tubitv/ui/TubiLoadingView.java
+++ lib/src/main/java/com/tubitv/ui/TubiLoadingView.java
@@ -1,34 +1,115 @@
 package com.tubitv.ui;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
 /**
+ * A simple rotating spinner with custom attributes to set duration of the rotation
+ * <p>
  * Created by stoyan on 4/24/17.
  */
 public class TubiLoadingView extends android.support.v7.widget.AppCompatImageView {
+    /**
+     * The default duration of the rotation animation {@link #mRotateAnim}
+     */
+    private static final int DEFAULT_ROTATION_DURATION_MS = 1000;
+
+    /**
+     * The duration of the the rotation animation {@link #mRotateAnim},
+     *
+     * @see #DEFAULT_ROTATION_DURATION_MS
+     */
+    private int mRotationDuration = DEFAULT_ROTATION_DURATION_MS;
+
+    /**
+     * The rotation animation that we apply to this view
+     */
+    private Animation mRotateAnim;
+
+    /**
+     * The state of whether this view. True if this is animating and visible
+     */
+    private boolean mIsRunning;
 
     public TubiLoadingView(Context context) {
         super(context);
+        init(null);
     }
 
     public TubiLoadingView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init(attrs);
     }
 
     public TubiLoadingView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    /**
+     * Initialize all of the images and animations as well as apply attributes if set
+     *
+     * @param attrs
+     */
+    private void init(@Nullable AttributeSet attrs) {
         setImageDrawable(getResources().getDrawable(R.drawable.ic_tubi_tv_loading_spinner));
 
-//        if (isInEditMode()) {
-//            return;
-//        }
+        if (isInEditMode()) {
+            return;
+        }
 
-        Animation rotateAnim = AnimationUtils.loadAnimation(getContext(), R.anim.tubi_tv_loading_spinner_rotate);
-//        rotateAnim.setRepeatMode(Animation.INFINITE);
-        startAnimation(rotateAnim);
+        if (attrs != null) {
+            TypedArray a = getContext().getTheme().obtainStyledAttributes(attrs,
+                    R.styleable.TubiLoadingView, 0, 0);
+            try {
+                mRotationDuration = a.getInt(R.styleable.TubiLoadingView_rotation_duration_ms, DEFAULT_ROTATION_DURATION_MS);
+            } finally {
+                a.recycle();
+            }
+        }
+
+        mRotateAnim = AnimationUtils.loadAnimation(getContext(), R.anim.tubi_tv_loading_spinner_rotate);
+        start();
+    }
+
+    /**
+     * Toggles the visibility and animation.
+     */
+    public void toggle() {
+        if (mIsRunning) {
+            stop();
+        } else {
+            start();
+        }
+    }
+
+    /**
+     * Stops the animation and makes the view gone
+     */
+    public void stop() {
+        if(!mIsRunning){//already stopped
+            return;
+        }
+        clearAnimation();
+        setVisibility(View.GONE);
+        mIsRunning = false;
+    }
+
+    /**
+     * Starts the animation and makes the view visible
+     */
+    public void start() {
+        if(mIsRunning){//already running
+            return;
+        }
+        mRotateAnim.setDuration(mRotationDuration);
+        setVisibility(View.VISIBLE);
+        startAnimation(mRotateAnim);
+        mIsRunning = true;
     }
 }
